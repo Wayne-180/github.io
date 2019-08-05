80c379eca9f0737f2e11cd39b2d7984541e10a45
Wayne <Wayne_180@foxmail.com>
2019-08-05 20:55:06 +0800

    test

--- miao/lodash/wayne18o-lodash.js
+++ miao/lodash/wayne18o-lodash.js
@@ -147,13 +147,13 @@ var wayne18o = {
     dropRightWhile: function (array, predicate) {
 
     },
-    isMatch: function isMatch(obj, src) {
+    isMatch: function (obj, src) {
         if (obj === src) {
             return true
         }
         for (var key in obj) {
             if (typeof src[key] == 'object' && src[key] !== null) {
-                if (!isMatch(obj[key], src[key])) {
+                if (!wayne_18o.isMatch(obj[key], src[key])) {
                     return false
                 } else {
                     return true
